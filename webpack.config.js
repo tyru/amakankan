49b24c70a3e10b566204b11ef52132e69739a5ac
r7kamura <r7kamura@gmail.com>
2017-01-31 13:40:45 +0900

    Remove unnecessary duplicate code

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ const plugins = [
 
 if (process.env.NODE_ENV === "production") {
   plugins.push(
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /ja/),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify("production"),
